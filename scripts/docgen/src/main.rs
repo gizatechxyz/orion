bb44b3bcb2a4f0f568c6010ecf39b4204f33c457
raphaelDkhn <raphael@gizatech.xyz>
2023-07-29 09:16:25 +0300

    move docgen to scripts folder

--- scripts/docgen/src/main.rs
+++ scripts/docgen/src/main.rs
@@ -4,40 +4,40 @@ use std::path::Path;
 
 fn main() {
     // TENSOR DOC
-    let trait_path = "src/operators/tensor/core.cairo";
-    let doc_path = "docs/framework/operators/tensor";
+    let trait_path = "../src/operators/tensor/core.cairo";
+    let doc_path = "../docs/framework/operators/tensor";
     let label = "tensor";
     let trait_name = "TensorTrait";
     doc_trait(trait_path, doc_path, label);
     doc_functions(trait_path, doc_path, trait_name, label);
 
     // NN DOC
-    let trait_path = "src/operators/nn/core.cairo";
-    let doc_path = "docs/framework/operators/neural-network";
+    let trait_path = "../src/operators/nn/core.cairo";
+    let doc_path = "../docs/framework/operators/neural-network";
     let label = "nn";
     let trait_name = "NNTrait";
     doc_trait(trait_path, doc_path, label);
     doc_functions(trait_path, doc_path, trait_name, label);
 
     // FIXED POINT DOC
-    let trait_path = "src/numbers/fixed_point/core.cairo";
-    let doc_path = "docs/framework/numbers/fixed-point";
+    let trait_path = "../src/numbers/fixed_point/core.cairo";
+    let doc_path = "../docs/framework/numbers/fixed-point";
     let label = "fp";
     let trait_name = "FixedTrait";
     doc_trait(trait_path, doc_path, label);
     doc_functions(trait_path, doc_path, trait_name, label);
 
     // SIGNED INTEGER DOC
-    let trait_path = "src/numbers/signed_integer/integer_trait.cairo";
-    let doc_path = "docs/framework/numbers/signed-integer";
+    let trait_path = "../src/numbers/signed_integer/integer_trait.cairo";
+    let doc_path = "../docs/framework/numbers/signed-integer";
     let label = "int";
     let trait_name: &str = "IntegerTrait";
     doc_trait(trait_path, doc_path, label);
     doc_functions(trait_path, doc_path, trait_name, label);
 
     // PERFORMANCE DOC
-    let trait_path = "src/performance/core.cairo";
-    let doc_path = "docs/framework/performance";
+    let trait_path = "../src/performance/core.cairo";
+    let doc_path = "../docs/framework/performance";
     let label = "performance";
     let trait_name = "PerfomanceTrait";
     doc_trait(trait_path, doc_path, label);
